d770202d643160d8d9790320f7adb8f2eb3a227f
MrPrincee <otar.jojishvili.1@btu.edu.ge>
2024-01-17 23:25:52 +0400

    .

--- src/models/certificate.py
+++ src/models/certificate.py
@@ -3,6 +3,7 @@ from src.extensions import db
 
 
 class Certificate(BaseModel):
+    uuid = db.Column(db.String)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     date = db.Column(db.Date)
