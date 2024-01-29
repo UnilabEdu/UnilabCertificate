d9a44dd4a3f43106bc5dbcf66b1322c219c01f3f
MrPrincee <otar.jojishvili.1@btu.edu.ge>
2024-01-29 21:57:39 +0400

    .

--- src/commands.py
+++ src/commands.py
@@ -12,8 +12,8 @@ from src.models import User
 @click.command("api_test")
 @with_appcontext
 def api_test():
-    # api/login
-    # link = "http://127.0.0.1:5000/api/login"
+    # # api/login
+    # link = "http://127.0.0.1:5000/users_id"
     # headers = {"Content-Type": "application/json"}
     # body = {"username": 'TestUser',"password":"TestUser"}
 
@@ -23,10 +23,10 @@ def api_test():
     #            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcwNTUxOTMyMSwianRpIjoiMjk1NjNjNWYtZjQ0Yi00Nzg0LWIxMWUtYjk4NzUwYzdkOGFiIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNzA1NTE5MzIxLCJjc3JmIjoiNzE1OGJiNTQtZWUzZC00NWVlLTlkOTctMTkxMjMzY2Y3ZDY1IiwiZXhwIjoxNzA1NTIwMjIxfQ.4VU4xqXAltG8P98xVVykQI4JPeL2wn_H-2qm53Yi_ME"}
     # body = {"username": 'TestUser', "date": '2004-12-01', "type": "TestType"}
 
-    # api/get_certificate
+
     link = "http://127.0.0.1:5000/api/get_certificate"
     headers = {"Content-Type": "application/json"}
-    body = {"uuid": "6de46d7f-79d4-429b-a6aa-4b1abd42de12"}
+    body = {"uuid": "0de8c984-c766-4ac3-ab0d-bc48fe711ce6"}
 
     received_response = requests.get(link, data=json.dumps(body), headers=headers)
     print(received_response.content)
