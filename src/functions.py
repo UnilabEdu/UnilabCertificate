7170b6299ba786e08afd589b6d88a0e414e966f6
MrPrincee <otar.jojishvili.1@btu.edu.ge>
2024-02-06 15:30:26 +0400

    .

--- src/functions.py
+++ src/functions.py
@@ -5,7 +5,8 @@ def certificate_text(subject, course_type):
     cert_text = f"მასზეთ რომ გაიარე  {subject} ის {course_type}\n ილიაუნის კიბერლაბორატორიაში\n ამიტომ იმიტომ"
     return cert_text
 
-def certificate_2d(username,type,subject,date):
+
+def certificate_2d(username, type, subject, date):
     image = Image.open('images/Certificate_blank.png')
 
     draw = ImageDraw.Draw(image)
@@ -20,8 +21,6 @@ def certificate_2d(username,type,subject,date):
     sign2 = Image.open("images/xelmowera.jpg")
     sign2 = sign.resize((200, 70))
 
-
-
     draw.text((580, 540), certificate_text(type, subject), fill=(0, 0, 0), font=font)
 
     draw.text((800, 380), username, fill=(0, 0, 0), font=font)
@@ -30,7 +29,6 @@ def certificate_2d(username,type,subject,date):
     #
     image.paste(sign, (930, 710))
     image.paste(sign, (930, 790))
-
 
     image.save(f"certificate_images/{str(username)}_certificate.jpg")
 
